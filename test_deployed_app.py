d8dd0ba715b279aeb3a583e5038b1992a73c7926
Manchanda <smancha5@optum.com>
2022-02-09 16:22:32 +0530

    add screenshots

--- test_deployed_app.py
+++ test_deployed_app.py
@@ -1,21 +1,25 @@
+"""
+A script to test the deployed app on heroku.
+"""
+
 import requests
 
 data = {
-        "age": 39,
-        "workclass": "ERROR",
-        "education": "Some-college",
-        "maritalStatus": "ERROR",
-        "occupation": "WRONG",
-        "relationship": "Husband",
+        "age": 28,
+        "workclass": "Private",
+        "education": "Bachelors",
+        "maritalStatus": "Married-civ-spouse",
+        "occupation": "Prof-specialty",
+        "relationship": "Wife",
         "race": "Black",
-        "sex": "Male",
-        "hoursPerWeek": 60,
-        "nativeCountry": "United-States"
+        "sex": "Female",
+        "hoursPerWeek": 40,
+        "nativeCountry": "Cuba"
     }
 
 r = requests.post('https://sahil-census-app.herokuapp.com/', json=data)
 
-# assert r.status_code == 200
+assert r.status_code == 200
 
 print(f"Response code: {r.status_code}")
 print(f"Response body: {r.json()}")
